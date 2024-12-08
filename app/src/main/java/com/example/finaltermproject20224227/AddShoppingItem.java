9715a1ce9b3f69389ae754d51bf0c7e7c6a06b30
selloriwoo <sellossier@gmail.com>
2024-12-08 15:17:46 +0900

    feature: 이름 변경

--- app/src/main/java/com/example/finaltermproject20224227/AddShoppingItem.java
+++ app/src/main/java/com/example/finaltermproject20224227/AddShoppingItem.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class addShoppingItem extends AppCompatActivity {
+public class AddShoppingItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
